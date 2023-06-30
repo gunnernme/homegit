7751b4c289b59946d3e5448df7e5135a9a0aebdc
eric olson <eric.a.olson@comcast.net>
2023-06-29 23:08:20 -0400

    more

--- ge/goesloc.py
+++ ge/goesloc.py
@@ -1,14 +1,29 @@
 import sys
+import re
 import json
 import requests
 import zipcodes
-import haversine
+from haversine import haversine, Unit
 from datetime import datetime,timedelta
 import config
+from pprint import PrettyPrinter, pprint
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 class Goesloc:
     def __init__(self):
-        self.goesdict = {}
+        self.goesdict = []
+        self.startzip = '01960'
+        self.max_asloc = config.max_asloc
+
+        try:
+            startlat = (zipcodes.matching(self.startzip)[0]['lat'])
+            startlong = (zipcodes.matching(self.startzip)[0]['long'])
+        except IndexError:
+            eprint("Bad zipcode")
+            exit(1)
+        self.startgeo = (float(startlat), float(startlong))
         self.fill_locations(self.goesdict)
 
     def fill_locations(self, goesdict):
@@ -22,11 +37,10 @@ class Goesloc:
             eprint("Bad response to http request GET: '{url}'".format(url=api_url))
             exit(1)
 
-        if len(out) >= int(config.max_asloc):
-            print("There are {ct} locations returned".format(ct=len(out)))
+        if len(out) >= int(self.max_asloc):
+            eprint("There are {ct} locations returned".format(ct=len(out)))
 
         for item in out:
-            key = item['id']
             if item['countryCode'] == 'US' and item['postalCode']:
                 zip = item['postalCode'][0:5]
                 test = zipcodes.matching(zip)
@@ -34,28 +48,96 @@ class Goesloc:
                     item['lat'] = test[0]['lat']
                     item['long'] = test[0]['long']
                     item['geo'] = (float(item['lat']), float(item['long']))
-                    #item['dist'] = int(haversine(item['geo'], startgeo, unit=Unit.MILES))
-            self.goesdict[key] = item
+                    item['dist'] = int(haversine(item['geo'], self.startgeo, unit=Unit.MILES))
+
+                #self.goesdict[str(item['id'])] = item
+                self.goesdict.append(item)
+
+        self.locations_returned = len(out)
+        self.locations_with_zip = len(self.goesdict)
+        eprint("Fill returns {all_loc} with {zip_loc} in USA".format(all_loc = self.locations_returned, zip_loc = self.locations_with_zip))
+
 
     def search_location(self, key):
         location = self.find_or_add_location(key)
         return location
 
-    def find_or_add_location(self, key):
-        if key.isnumeric():
-            location = getlocation(id=key)
-        elif key.find("*") or key.find("?"):
-            location = getlocation(pat=key)
-        else:
-            location = getlocation(name=key)
+    def regex_find(self, pat):
+        return self.regex_find_by_field(name=pat)
 
-    def getlocation(self, key):
-        return self.goesdict[key]
+    def regex_find_by_field(self, **kwargs):
+        arr = []
+        result = []
+
+        for type in kwargs:
+            value = kwargs[type]
+            eprint("Type " + type + " value " + value)
+            arr.append((type, value))
+
+        (type, value) = arr[0]
+
+        reg = re.compile(value)
+        eprint("Type " + type + " value " + value)
+        for i in self.goesdict:
+            if reg.match(i[type]):
+                result.append(i)
+
+        return result
+
+    def find_by_field(self, **kwargs):
+        arr = []
+        result = []
+
+        for type in kwargs:
+            value = kwargs[type]
+            eprint("Type " + type + " value " + value)
+            arr.append((type, value))
+
+        (type, value) = arr[0]
+
+        eprint("Type " + type + " value " + value)
+        for i in self.goesdict:
+            if str(i[type]) == str(value):
+                result.append(i)
+
+        return result
+
+
+    def find_or_add_location(self, key):
+        location = None
+        skey = str(key)
+        if skey.isnumeric():
+            arr = self.find_by_field(id=skey)
+        elif skey.find("*") or skey.find("?"):
+            arr = self.regex_find(skey)
+        else:
+            arr = self.find_by_field(name=skey)
+        self.udump(arr)
+        if arr:
+            location = arr[0]
+
+        return location
+
 
     def dump(self):
-        print(self.goesdict)
+        pp = PrettyPrinter(indent=4)
+        pp.pprint(self.goesdict)
+
+    def udump(self, object):
+        pp = PrettyPrinter(indent=4)
+        pp.pprint(object)
 
 # ---------------
 x = Goesloc()
 
-x.dump()
+loc = x.search_location('5003')
+x.udump(loc)
+loc = x.search_location('.*erb.*')
+x.udump(loc)
+loc = x.search_location('.*radley.*port.*')
+x.udump(loc)
+loc = x.search_location('88888')
+if loc is not None:
+    x.udump(loc)
+else:
+    print("not foudn")
